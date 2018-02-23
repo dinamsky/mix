e0818c1a3d528f8f697c56b68b07cf7265c69950
Timur Malyshev <wqs-info@mail.ru>
2018-02-23 14:42:36 +0500

    paypal test 2

--- src/AppBundle/Controller/PayPalController.php
+++ src/AppBundle/Controller/PayPalController.php
@@ -14,43 +14,12 @@ use Symfony\Component\HttpFoundation\Response;
 class PayPalController extends Controller
 {
     /**
-     * @Route("/paypalCreatePayment", name="createPayment")
+     * @Route("/paypalIPN", name="createPayment")
      */
     public function indexAction(Request $request)
     {
 
-        $data = '{
-          "intent":"sale",
-          "redirect_urls":{
-            "return_url":"https://mix.rent/return_url",
-            "cancel_url":"https://mix.rent/cancel_url"
-          },
-          "payer":{
-            "payment_method":"paypal"
-          },
-          "transactions":[
-            {
-              "amount":{
-                "total":"7.47",
-                "currency":"USD"
-              },
-              "description":"This is the payment transaction description."
-            }
-          ]
-        }';
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-          "Content-Type: application/json",
-          "Authorization: Bearer EOjEJigcsRhdOgD7_76lPfrr45UfuI43zzNzTktUk1MK",
-          "Content-length: ".strlen($data))
-        );
-
-        $get = curl_exec($ch);
-
-        dump($get);
+        file_put_contents('IPN', json_encode($request));
 
         return new Response();
     }
@@ -74,6 +43,32 @@ class PayPalController extends Controller
             'generalTypes' => $generalTypes,
             'in_city' => $in_city,
             'lang' => $_SERVER['LANG'],
+        ]);
+    }
+
+    /**
+     * @Route("/paypalTestPage", name="createPayment")
+     */
+    public function paypalTestPageAction(Request $request, EntityManagerInterface $em)
+    {
+
+
+        $query = $em->createQuery('SELECT g FROM AppBundle:GeneralType g WHERE g.total !=0 ORDER BY g.weight, g.total DESC');
+        $generalTypes = $query->getResult();
+
+        $city = $this->get('session')->get('city');
+
+        $in_city = $city->getUrl();
+
+        $customData = ['user_id' => 1, 'product_id' => 5];
+
+       return $this->render('paypal/test_page.html.twig', [
+            'city' => $city,
+            'cityId' => $city->getId(),
+            'generalTypes' => $generalTypes,
+            'in_city' => $in_city,
+            'lang' => $_SERVER['LANG'],
+           'customData' => json_encode($customData)
         ]);
     }
 }
